62fed77d86edd9da7c9798aa587b5bf7e0d9803b
Ahmed Mahfouz Ezzat <ahmedazzat121@gmail.com>
2019-04-02 06:00:52 +0200

    start up code
    add project files only

---
+++ Can Car Project_Node1/Can Car Project_Node1.c
@@ -0,0 +1,18 @@
+/*
+ * Can_Car_Project.c
+ *
+ * Created: 30/03/2019 09:44:27 PM
+ *  Author: Ahmed
+ */ 
+
+#include <avr/io.h>
+#include "mcp2515.h"
+
+int main(void)
+{
+    while(1)
+    {
+        
+		
+    }
+}
---
+++ Can Car Project_Node2/Can Car Project_Node2.c
@@ -0,0 +1,17 @@
+/*
+ * Can_Car_Project_Node2.c
+ *
+ * Created: 02/04/2019 05:03:46 AM
+ *  Author: Ahmed
+ */ 
+
+#include <avr/io.h>
+#include "mcp2515.h"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
